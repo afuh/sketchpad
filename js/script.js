8ad0f8ba840643939d56b1d288b6e2d9fbdf5623
Axel <axelfuh@gmail.com>
2017-02-16 21:17:35 +0100

    Second draft

--- js/script.js
+++ js/script.js
@@ -1,41 +1,77 @@
-$(document).ready(function() {
-  var userInput = 64,
-      total = userInput*userInput,
-      medida = 960/userInput;
 
-  for (var i = 0; i < total; i++) {
-   $(".sketch").prepend("<div class=\"square\"></div>");
- }
+var userInput = 32;
 
-  $(".square").css({width: + medida + "px", height: + medida + "px"})
-              .mouseenter(function(){
-                $(this).addClass("square-hover");
-                ($(this).css("opacity") <= 0.9) ? $(this).css("opacity", "+=0.1") : $(this).css("opacity", "1");
-              });
+$(document).ready(function(){
+  canvas();
+  paint();
+  $("#blackWhite").hide();
+});
+
+//populate the Canvas
+var canvas = function() {
+  $("#sketch").html("<div id=\"canvas\"></div>");
+
+  for (var i = 0; i < userInput*userInput; i++) {
+    $("#canvas").prepend("<div class=\"square\"></div>");
+  }
+   $(".square").css({width: + 960/userInput + "px", height: + 960/userInput + "px"});
+};
+
+// Painting & opacity
+var paint = function() {
+  $(".square").mouseenter(function(){
+              $(this).addClass("square-hover");
+              $(this).css("opacity") <= 0.9 ? $(this).css("opacity", "+=0.1") : $(this).css("opacity", "1");
+              }); 
+};
+
+//change the amount of squares
+var userCanvas = function(){
+  if (+$("#rowsColumns").val() <= 100 && +$("#rowsColumns").val() > 0) {
+    userInput = $("#rowsColumns").val();
+    $("#canvas").remove();
+    canvas();
+    paint();
+  }
+  else {
+    console.log("nop");
+    //texto que diga que no se puede
+  }
+};
+
+//Color
+var rainbow = function() {
+  $("#rainbow").hide();
+  $("#blackWhite").show();
+
+  $(".square").mouseenter(function(){
+    var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+
+    $(this).css("background", hue);
+  });
+};
+
+//Black and White
+var blackWhite = function() {
+  $("#blackWhite").hide();
+  $("#rainbow").show();
+
+  $(".square").mouseenter(function(){
+    $(this).css("background", "black");
+  });
+};
+
+
 
 
 
 
 /*
-$(".reset").click(function(){
-  $("input").fadeToggle(500);
-});
-
-
-$("form").submit(function() {
-    var input = $("input").val();
-    console.log(input);
-  });
-
 var pete = function() {
-    $(".sketch").fadeTo(2000, 0.6, function(){
-      $(".sketch").fadeTo(4000, 1);
+    $("#sketch").fadeTo(2000, 0.6, function(){
+      $("#sketch").fadeTo(4000, 1);
       return pete();
     });
 };
 pete();
 */
-
-
-
-});
