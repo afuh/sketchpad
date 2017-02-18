52dd73cc5a0899415370119c9a89a3acdfc6e12e
Axel <axelfuh@gmail.com>
2017-02-18 01:01:06 +0100

    Third (and last) draft

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
-
-var userInput = 32;
+var userInput = 64,
+    color;
 
 $(document).ready(function(){
+  $("#blackWhite").hide();
+  $("#go").hide();
+  color = false;
   canvas();
   paint();
-  $("#blackWhite").hide();
 });
 
-//populate the Canvas
+//populate the canvas
 var canvas = function() {
   $("#sketch").html("<div id=\"canvas\"></div>");
 
   for (var i = 0; i < userInput*userInput; i++) {
     $("#canvas").prepend("<div class=\"square\"></div>");
   }
+
    $(".square").css({width: + 960/userInput + "px", height: + 960/userInput + "px"});
 };
 
@@ -22,48 +25,97 @@ var paint = function() {
   $(".square").mouseenter(function(){
               $(this).addClass("square-hover");
               $(this).css("opacity") <= 0.9 ? $(this).css("opacity", "+=0.1") : $(this).css("opacity", "1");
+
+              // TODO: spider $(this).fadeTo(400, 0);
+
               }); 
 };
 
-//change the amount of squares
+//change the amount of pixels
 var userCanvas = function(){
-  if (+$("#rowsColumns").val() <= 100 && +$("#rowsColumns").val() > 0) {
-    userInput = $("#rowsColumns").val();
+  if (+$("#inputBox").val() <= 100 && +$("#inputBox").val() > 0) {
+    userInput = $("#inputBox").val();
     $("#canvas").remove();
     canvas();
     paint();
+    if (color && +$("#shape").val() === 0) {
+      rainbow(true);
+      shape(true);
+    }
+    else if (color) {
+      rainbow(true);
+    }
+    else if (+$("#shape").val() === 0) {
+      shape(true);
+    }
   }
   else {
-    console.log("nop");
-    //texto que diga que no se puede
+    $("#nop").fadeTo(400, 1, function(){
+      $(this).fadeTo(4000, 0);
+    });
   }
 };
 
-//Color
-var rainbow = function() {
-  $("#rainbow").hide();
-  $("#blackWhite").show();
+//Color and B&W
+var rainbow = function(b) {
+  if (!color || b) {
+    color = true;
+    $("#rainbow").text("B&W");
 
-  $(".square").mouseenter(function(){
-    var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+    $(".square").mouseenter(function(){
+      var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
 
-    $(this).css("background", hue);
-  });
+      $(this).css("background", hue);
+    });
+  }
+  else {
+    color = false;
+    $("#rainbow").text("Rainbow");
+
+    $(".square").mouseenter(function(){
+      $(this).css("background", "black");
+    });
+  }
 };
 
-//Black and White
-var blackWhite = function() {
-  $("#blackWhite").hide();
-  $("#rainbow").show();
-
-  $(".square").mouseenter(function(){
-    $(this).css("background", "black");
-  });
+//reset canvas
+var resetCanvas = function(){
+  $("#canvas").remove();
+  canvas();
+  paint();
+  if (color && +$("#shape").val() === 0) {
+    rainbow(true);
+    shape(true);
+  }
+  else if (color) {
+    rainbow(true);
+  }
+  else if (+$("#shape").val() === 0) {
+    shape(true);
+  }
 };
 
+// go button
+$(function() {
+  $("input").mousedown(function(){
+      $("#go").fadeIn("fast");
+  });
+    $("#go").click(function(){
+      $(this).fadeOut("fast");
+    });
+});
 
-
-
+//shape
+var shape = function(b) {
+  if (+$("#shape").val() == 1 || b) {
+    $("#shape").val(0);
+    $(".square").addClass("roundPixel").removeClass("squarePixel");
+  }
+  else {
+    $("#shape").val(1);
+    $(".square").addClass("squarePixel").removeClass("roundPixel");
+  }
+};
 
 
 /*
